6c8786d5e7449fb2fe339e8663ab830abc7c0add
Eric Long <Eric Long>
2017-05-17 20:21:35 -0700

    Finished base mechanices. Just need some touching up. Working.

--- build-tmp/source/CircuitSimulator.java
+++ build-tmp/source/CircuitSimulator.java
@@ -18,6 +18,7 @@ float totalResistance;
 float rest23 = 0;
 float volt23 = 0;
 float totalCurrent;
+int additional=1;
 Resistor resistor1 = new Resistor();
 Resistor resistor2 = new Resistor();
 Resistor resistor3 = new Resistor();
@@ -26,6 +27,19 @@ Battery duracell = new Battery();
 public void setup()
 {
 	
+}
+public void mouseClicked()
+{
+	if(mouseX>150&&mouseX<350&&mouseY>125&&mouseY<225){resistor2.resistance+=additional;}
+	if(mouseX>650&&mouseX<850&&mouseY>125&&mouseY<225){resistor3.resistance+=additional;}
+	if(mouseX>150&&mouseX<350&&mouseY>375&&mouseY<475){resistor1.resistance+=additional;}
+	if(mouseX>650&&mouseX<850&&mouseY>375&&mouseY<475){resistor4.resistance+=additional;}
+	if(mouseX>400&&mouseX<600&&mouseY>375&&mouseY<475){duracell.voltage+=additional;}
+}
+public void keyPressed()
+{
+	if(key == '1'){additional = 1;}
+	if(key == '2'){additional = -1;}
 }
 public void draw()
 {
@@ -36,7 +50,7 @@ public void draw()
 	resistor1.voltage = resistor1.resistance * totalCurrent;
 	volt23 = rest23* totalCurrent;
 	resistor2.current = volt23 / resistor2.resistance;
-	resistor3.current = volt23 / resistor2.resistance;
+	resistor3.current = volt23 / resistor3.resistance;
 	resistor2.voltage = volt23;
 	resistor3.voltage = volt23;
 	resistor4.current = totalCurrent;
@@ -59,7 +73,7 @@ public void draw()
 	text("Resistor 1",250,390);
 	text("Battery",500,390);
 	text("Resistor 4",750,390);
-	textSize(10);
+	textSize(15);
 	text(resistor2.resistance+" Ohm",250,165);
 	text(resistor2.current+" Amps",250,185);
 	text(resistor2.voltage+" Volts",250,205);
@@ -69,23 +83,19 @@ public void draw()
 	text(resistor3.resistance+" Ohm",750,165);
 	text(resistor3.current+" Amps",750,185);
 	text(resistor3.voltage+" Volts",750,205);
-	text(resistor2.resistance+" Ohm",250,415);
-	text(resistor2.current+" Amps",250,435);
-	text(resistor2.voltage+" Volts",250,455);
-	text(rest23+" Ohm",500,415);
-	text(totalCurrent+" Amps",500,435);
-	text(volt23+" Volts",500,455);
-	text(resistor3.resistance+" Ohm",750,415);
-	text(resistor3.current+" Amps",750,435);
-	text(resistor3.voltage+" Volts",750,455);
+	text(resistor1.resistance+" Ohm",250,415);
+	text(resistor1.current+" Amps",250,435);
+	text(resistor1.voltage+" Volts",250,455);
+	text(resistor4.resistance+" Ohm",750,415);
+	text(resistor4.current+" Amps",750,435);
+	text(resistor4.voltage+" Volts",750,455);
+	textSize(30);
+	text(duracell.voltage+" Volts",500,440);
 }
 public float calTotRes()
 {
 	rest23 = 1/((1/resistor2.resistance)+(1/resistor3.resistance));
 	return resistor1.resistance + rest23 + resistor4.resistance;
-}
-public void showInfoBox()
-{
 }
 class Resistor
 {
